938fb2527ad5dc0c83c59bfa0ec30863c651627c
realnakrul <21193002+realnakrul@users.noreply.github.com>
2019-02-09 10:31:17 +0400

    Task 2 completed

---
+++ m2/m2/m2.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "math.h"
+using namespace std;
+
+int main()
+{	
+	int n;
+	double a, e, result;
+	e = pow(10, -4);
+	a = 0;
+	result = 0;
+	n = 1;
+	cout << fixed;
+ 	do
+	{
+		a = (1 / pow(2, n)) + (1 / pow(3, n));
+		result += a;
+		cout << n << ' ' << a << ' ' << e << ' ' << result <<'\n';
+		n++;
+	} while (a >= e);
+}
